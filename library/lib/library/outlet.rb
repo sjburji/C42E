453b78a88a54264ad0e8332012effb0354a296cb
Your Name <supreethjburji@yahoo.com>
2011-07-24 11:30:47 +0530

    added issue spec

--- library/lib/library/outlet.rb
+++ library/lib/library/outlet.rb
@@ -4,8 +4,8 @@ module Library
 		def initialize
 			@books = Array.new
 			@books = [{:book => 'BOOK1', :author => 'AUTHOR1', :isbn => 'ISBN1', :cost => 10},
-				 {:book => 'BOOK2', :author => 'AUTHOR2', :isbn => 'ISBN2', :cost => 10},
-				 {:book => 'BOOK3', :author => 'AUTHOR3', :isbn => 'ISBN3', :cost => 10}]
+				 {:book => 'BOOK2', :author => 'AUTHOR2', :isbn => 'ISBN2', :cost => 20},
+				 {:book => 'BOOK3', :author => 'AUTHOR3', :isbn => 'ISBN3', :cost => 30}]
 		end
 		
 		def books
@@ -18,6 +18,14 @@ module Library
 					return book
 				end
 			end
+			return nil
+		end
+
+		def issue(isbn)
+			unless lookup(isbn).nil?
+				return true
+			end
+			return false
 		end
 	end
 end
--- library/spec/library/outlet_spec.rb
+++ library/spec/library/outlet_spec.rb
@@ -6,13 +6,30 @@ module Library
 			@outlet = Outlet.new()
 		end
 
-		it "should have 3 books" do
-			@outlet.books.should eq(3)
+		context "Outlet" do
+			it "should have 3 books" do
+				@outlet.books.should eq(3)
+			end
+
+			it "should give Book1 when looked up with ISBN1" do
+				book = {:book => 'BOOK1', :author => 'AUTHOR1', :isbn => 'ISBN1', :cost => 10}
+				@outlet.lookup('ISBN1').should eq(book)
+			end
+
+			it "should return nil when book not found" do
+				@outlet.lookup('ISBN8').should eq(nil)
+			end
 		end
 
-		it "should give Book1 when looked up with ISBN1" do
-			book = {:book => 'BOOK1', :author => 'AUTHOR1', :isbn => 'ISBN1', :cost => 10}
-			@outlet.lookup('ISBN1').should eq(book)
+		context "Customer" do
+
+			it "should have book issued if found" do
+				@outlet.issue('ISBN1').should eq(true)
+			end
+
+			it "should not have book issued if not found" do
+				@outlet.issue('ISBN8').should eq(false)
+			end
 		end
 	end
 end
